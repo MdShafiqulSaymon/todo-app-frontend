c9c3cdac64e2c8850b523482fb6f2bb7c2647e52
Md. Shafiqul Hasan <mdshafiqulsaymon@gmail.com>
2025-05-23 17:30:39 +0600

    Sync with Backend APi

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEST_PUBLIC_API_URL: 'http://localhost:3000',
+    NEST_PUBLIC_API_URL: 'https://todo-app-production-8c59.up.railway.app',
   },
 };
 
